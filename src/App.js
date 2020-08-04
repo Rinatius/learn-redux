24458aa6463ff74ea34457e5f829ab29749b9084
Rinatius <rinat@kloop.kg>
2020-08-04 09:40:38 +0600

    Add store function

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Crement from './components/Crement/Crement'
+import Button from "@material-ui/core/Button";
 
 
 class App extends React.Component {
 
   state = {
-    counter: 0
+    counter: 0,
+    results: []
   }
+
   handleCrementClick = (number) => {
     this.setState({counter: this.state.counter + number})
   };
 
+  handleStoreClick = (count) => {
+    this.setState({results: this.state.results.concat(count)})
+  }
 
   render() {
     return (
@@ -22,6 +28,11 @@ class App extends React.Component {
                    label="Standard"
                    value={this.state.counter}/>
         <Crement onCrClick={this.handleCrementClick}/>
+        <p/>
+        <Button variant="contained" onClick={() => this.handleStoreClick(this.state.counter)}>STORE</Button>
+        <ul>
+          {this.state.results.map(r => <li>{r}</li>)}
+        </ul>
       </div>
     );
   }
